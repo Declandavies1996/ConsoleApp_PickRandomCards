adf9ec9a311955fc9db4a8b7e7057466aebe50b9
decla <decla@DECLAN>
2024-01-22 23:49:59 +0000

    Add project files.

---
+++ PickRandomCards/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace PickRandomCards
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number of cards to pick: ");
+            string? line = Console.ReadLine();
+
+            if(int.TryParse(line, out int numberOfCards))
+            {
+                string[] cards = CardPicker.PickSomeCards(numberOfCards);
+                foreach(string card in cards)
+                {
+                    Console.WriteLine(card);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Please Enter A Valid Number.");
+            }
+        }
+    }
+}
+
